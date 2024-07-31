6227fdeaa23e57e8a0b8c5feb1109d108c3c9b06
wjx0820 <1989wjx@gmail.com>
2024-07-31 15:55:06 +0800

    loading posts

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,23 @@
 import { clsx, type ClassValue } from "clsx"
+import { formatDate, formatDistanceToNowStrict } from "date-fns"
 import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+export function formatRelativeDate(from: Date) {
+  const currentDate = new Date()
+  // If the date is within the last 24 hours, show the relative time
+  if (currentDate.getTime() - from.getTime() < 24 * 60 * 60 * 1000) {
+    return formatDistanceToNowStrict(from, { addSuffix: true })
+  } else {
+    // If the date is within the current year, only show the month and day
+    if (currentDate.getFullYear() === from.getFullYear()) {
+      return formatDate(from, "MMM d")
+    } else {
+      // Otherwise, show the full date
+      return formatDate(from, "MMM d, yyyy")
+    }
+  }
 }
