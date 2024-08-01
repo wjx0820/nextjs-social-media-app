b68d33f9e540d0630985c7749ae0c4cf02e91d45
wjx0820 <1989wjx@gmail.com>
2024-08-01 14:33:43 +0800

    trending hash tag

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -2,6 +2,7 @@ import Image from "next/image"
 
 import PostEditor from "@/components/posts/editor/PostEditor"
 import Post from "@/components/posts/Post"
+import TrendsSidebar from "@/components/TrendsSidebar"
 import prisma from "@/lib/prisma"
 import { postDataInclude } from "@/lib/types"
 
@@ -14,13 +15,14 @@ export default async function Home() {
   })
 
   return (
-    <main className="w-full min-w-0">
+    <main className="flex w-full min-w-0 gap-5">
       <div className="w-full min-w-0 space-y-5">
         <PostEditor />
         {posts.map((post) => (
           <Post key={post.id} post={post} />
         ))}
       </div>
+      <TrendsSidebar />
     </main>
   )
 }
